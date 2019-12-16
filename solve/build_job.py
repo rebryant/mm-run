299c0e329be9f648ebd00df6ef66043766d39797
Randal E. Bryant <Randy.Bryant@cs.cmu.edu>
2019-12-16 10:53:59 -0500

    Added memory option

--- solve/build_job.py
+++ solve/build_job.py
@@ -2,19 +2,21 @@
 
 #Generate job for specified level combination
 import sys
+import getopt
 
 def usage(name):
-    print("Usage: %s L1 L2 [SECS]")
-    print("  L1, L2: Levels being merged")
-    print("  SECS: Time limit in seconds")
-    
+    print("Usage: %s [-h] [-t SECS] [-g GBYTES] L1 L2 ")
+    print("  L1, L2:   Levels being merged")
+    print(" -h:        This message")
+    print(" -t SECS:   Time limit in seconds")
+    print(" -g GBYTES: Max gigabytes")
 
 def root(level1, level2):
     if level1 < level2:
         level1, level2 = level2, level1
     return "m%.2d+%.2d" % (level1, level2)
 
-def generate(level1, level2, seconds):
+def generate(level1, level2, seconds, gigabytes):
     rname = root(level1, level2)
     jname = "run-%s.job" % rname
     outf = open(jname, 'w')
@@ -28,22 +30,33 @@ def generate(level1, level2, seconds):
     outf.write('#SBATCH -t %d:%.2d:%.2d\n' % (hours, minutes, seconds))
     outf.write('#SBATCH --ntasks-per-node=28\n')
     outf.write('cd /pylon5/cc5piip/rebryant/mm-run/solve\n')
-
+    megabytes = gigabytes * 1024
     froot = 'run-smirnov-%s-mode2' % rname
-    outf.write('/home/rebryant/Cloud-BDD/runbdd -v 1 -f %s.cmd -L %s.log\n' % (froot, froot))
+    outf.write('/home/rebryant/mm-run/Cloud-BDD/runbdd -v 1 -M %d -f %s.cmd -L %s-G%d.log\n' % (megabytes, froot, froot, gigabytes))
     outf.close()
 
 
 def run(name, args):
     seconds = 1800
-    if len(args) < 2 or len(args) > 3:
+    gigabytes = 32
+    optlist, args = getopt.getopt(args, 'ht:g:')
+    for (opt, val) in optlist:
+        if opt == '-h':
+            usage(name)
+            return
+        elif opt == '-t':
+            seconds = int(val)
+        elif opt == '-g':
+            gigabytes = int(val)
+        else:
+            usage(name)
+            return
+    if len(args) != 2:
         usage(name)
         return
     level1 = int(args[0])
     level2 = int(args[1])
-    if len(args) == 3:
-        seconds = int(args[2])
-    generate(level1, level2, seconds)
+    generate(level1, level2, seconds, gigabytes)
 
 if __name__ == "__main__":
     run(sys.argv[0], sys.argv[1:])
